8da532749f34707b94e434a4c94cda909ac1354f
yourtechsis <torir.adams@gmail.com>
2018-11-26 01:54:34 -0700

    authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,20 +4,25 @@ const router = express.Router();
 const User = require ('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+
+
 // like res.send just with json
 // get the users
-router.get('/', (req, res) => res.json( {msg: 'users work'} ));
+router.get('/test', (req, res) => res.json( {msg: 'users work'} ));
 
-//create a new user
+//create a new user. mongoose finds if the user exist. takes the user model 
+//gets the user if there is one based on the email and returns the message, if not gets the user model to create a new user
+// user is a param
 router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email }).then(user => {
         if(user){
-            return res.status(400).json({ email: "Email exists"})
+            return res.status(400).json({ email: "Email exists"});
         } else {
+
             const avatar = gravatar.url(req.body.email, {
-             s: '200',
+             s: '200', //size
              r: 'pg',
-             d: 'mm'
+             d: 'mm'  //defualt
 
             });
             const newUser = new User({
@@ -35,10 +40,43 @@ router.post('/register', (req, res) => {
                     .save()
                     .then(user => res.json(user))
                     .catch(err => console.log(err));
-                })
-            })
+                });
+            });
+        }
+    });
+    // api/users/login 
+    // will be returning the token
+  
+} );
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email
+    User.findOne({email}).then(user => {
+        // checks user
+    if(!user){
+        return res.status(404).json({email: 'user not found'});
+    }
+    // check password plain txt against hast using compare method
+    // bcrypt.compare(password, user.password)
+    // .then(isMatch => {
+    //     if(isMatch) {
+    //         res.json({msg: 'success'});
+    // } else {
+    //     return res.staus(404).json({password: "pw exists"});
+    // }
+    bcrypt.compare(password, user.password).then(isMatch => {
+        if(isMatch){
+            res.json({msg: "success"});
+        }else{
+    
+
+    return res.status(400).json({password: "incorrect pw"});
         }
     })
-} );
+})
+})
 
 module.exports = router;
